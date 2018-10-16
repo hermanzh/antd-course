09d9e342ac5a4d768f7cdf8ff072993a669567c2
Zhang Long <longxing386@163.com>
2018-10-16 16:29:30 +0800

    route第一版

--- config/config.js
+++ config/config.js
@@ -5,14 +5,27 @@ export default {
     component: '../layout/index',
     routes: [
       {
-        path: './helloworld',
+        path: '/helloworld',
         component: './HelloWorld',
+      },
+      {
+        path: '/',
+        component: './HelloWorld',
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
+        ]
       },
     ]
   }],
   plugins: [
     ['umi-plugin-react', {
-      antd: true
+      antd: true,
+      dva: true,
     }],
   ],
 };
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,17 +1,41 @@
 import { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+import Link from 'umi/link'
 
 // Header, Footer, Sider, Content 组件在Layout组件模块下
 const { Header, Footer, Sider, Content } = Layout;
+
+// 引入子菜单
+const SubMenu = Menu.SubMenu;
 
 export default class BasicLayout extends Component {
     render() {
         return (
             <Layout>
-                <Sider width={256} style={{ minHeight: '100vh', color: 'white' }}>Sider</Sider>
+                <Sider width={256} style={{ minHeight: '100vh'}}>
+                  <div style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px'}}/>
+                  <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">
+                      <Icon type="pie-chart" />
+                      <span>HelloWorld</span>
+                    </Menu.Item>
+                    <SubMenu 
+                      key="sub1" 
+                      title={<span><Icon type="dashboard"/><sapn>Dashboard</sapn></span>}
+                    >
+                        <Menu.Item key="2"><Link to="/dashboard/analysis">分析页</Link></Menu.Item>
+                        <Menu.Item key="3"><Link to="/dashboard/monitor">监控页</Link></Menu.Item>
+                        <Menu.Item key="4"><Link to="/dashboard/workplace">工作台</Link></Menu.Item>
+                    </SubMenu>
+                  </Menu>
+                </Sider>
                 <Layout>
                     <Header style={{ background: '#fff', textAlign: 'center', padding: 0 }}>Header</Header>
-                    <Content style={{ margin: '24px 16px 0' }}>{ this.props.children }</Content>
+                    <Content style={{ margin: '24px 16px 0' }}>
+                      <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                        {this.props.children}
+                      </div>
+                    </Content>
                     <Footer style={{ textAlign: 'center' }}>©2018 Hermanzh</Footer>
                 </Layout>
             </Layout>
